67da7b42b6b5872977a0d6418ca87ff0fc33437e
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-07-04 15:30:51 -0500

    icon and images

---
+++ src/utils/scripts/icons.js
@@ -0,0 +1,47 @@
+import htmlLogo from "../img/html5-original.svg";
+import cssLogo from "../img/css3-original.svg";
+import jsLogo from "../img/javascript-original.svg";
+import vueLogo from "../img/vuejs-original.svg";
+import reactLogo from "../img/react-original.svg";
+import nodeLogo from "../img/nodejs-original.svg";
+import firebaseLogo from "../img/firebase-original.svg";
+import mongoLogo from "../img/mongodb-original.svg";
+import mysqlLogo from "../img/mysql-original.svg";
+import figmaLogo from "../img/figma-original.svg";
+import materialLogo from "../img/materialui-original.svg";
+import angularLogo from "../img/angular-original.svg";
+import androidLogo from "../img/androidstudio-original.svg";
+import flutterLogo from "../img/flutter-original.svg";
+import tsLogo from "../img/typescript-original.svg";
+import trelloLogo from "../img/trello-original.svg";
+import notionLogo from "../img/notion-original.svg";
+import dockerLogo from "../img/docker-original.svg";
+import postmanLogo from "../img/postman-original.svg";
+import profilePhoto from "../img/JDiazc0.jpeg";
+import mineSweeperScreen from "../img/mine_sweeper.jpeg";
+
+const icon = {
+  htmlLogo,
+  cssLogo,
+  jsLogo,
+  vueLogo,
+  reactLogo,
+  nodeLogo,
+  firebaseLogo,
+  mongoLogo,
+  mysqlLogo,
+  figmaLogo,
+  materialLogo,
+  angularLogo,
+  androidLogo,
+  flutterLogo,
+  tsLogo,
+  trelloLogo,
+  notionLogo,
+  dockerLogo,
+  postmanLogo,
+  profilePhoto,
+  mineSweeperScreen,
+};
+
+export default icon;
